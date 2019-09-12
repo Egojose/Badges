cbe8e4dd4de585d794563a3fcd55c3c459d6eeb7
Egojose <josemicrobiologo@gmail.com>
2019-09-12 16:22:13 -0500

    setup inicial

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css';
 
-import './global.css';
-import App from './components/App';
-
+const element = <h1>Hello platzi from react!</h1>;
 const container = document.getElementById('app');
 
-ReactDOM.render(<App />, container);
+ReactDOM.render(element, container);
+
+
+
+// const element = document.createElement('h1');
+// element.innerText = 'Hello, platzi!';
+
+// const container = document.getElementById('app');
+
+// container.appendChild(element)
