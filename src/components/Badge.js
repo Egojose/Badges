12f3e2634e37ea1a1e9f405bf3f6354ba95ac295
Egojose <josemicrobiologo@gmail.com>
2019-09-12 17:57:46 -0500

    se da estilos a componentes

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react';
+import './styles/Badge.css';
+import confLogo from '../images/badge-header.svg';
 
 class Badge extends Component {
-    render() {
-        return <h1>Badge</h1>
-    }
+  render() {
+    return (
+      <div className="Badge">
+        <div className="Badge__header">
+          <img src={confLogo} alt="Logo conferencia" />
+        </div>
+
+        <div className="Badge__section-name">
+          <img src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar"  className="Badge__avatar"/>
+          <h1>
+            Jose <br /> Vásquez
+          </h1>
+        </div>
+
+        <div className="Badge__section-info">
+          <h3>Microbiologist</h3>
+          <div>@jose</div>
+        </div>
+
+        <div className="Badge__footer">#Platziconf</div>
+      </div>
+    );
+  }
 }
 
 export default Badge;
