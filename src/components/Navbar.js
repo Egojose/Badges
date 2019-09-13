53ded23c9874389db98c09c35929291f2e75ace7
Egojose <josemicrobiologo@gmail.com>
2019-09-13 09:46:08 -0500

    se crean nuevos componentes como navbar y badgenew

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,17 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react';
+import './styles/Navbar.css'
+import logo from '../images/logo.svg'
 
-import './styles/Navbar.css';
-import logo from '../images/logo.svg';
-
-class Navbar extends React.Component {
-  render() {
+class Navbar extends Component {
+  render () {
     return (
       <div className="Navbar">
         <div className="container-fluid">
-          <Link className="Navbar__brand" to="/">
-            <img className="Navbar__brand-logo" src={logo} alt="Logo" />
+          <a className="Navbar__brand" href="/">
+            <img src={logo} alt="Logo" className="Navbar__brand-logo" />
             <span className="font-weight-light">Platzi</span>
             <span className="font-weight-bold">Conf</span>
-          </Link>
+          </a>
         </div>
       </div>
     );
--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -1,33 +1,15 @@
-import React from 'react';
-
-import './styles/BadgeNew.css';
-import header from '../images/badge-header.svg';
+import React, { Component } from 'react';
 import Badge from '../components/Badge';
 import BadgeForm from '../components/BadgeForm';
+import header from '../images/badge-header.svg';
+import './styles/BadgeNew.css'
+import Navbar from '../components/Navbar';
 
-class BadgeNew extends React.Component {
-  state = {
-    form: {
-      firstName: '',
-      lastName: '',
-      email: '',
-      jobTitle: '',
-      twitter: '',
-    },
-  };
-
-  handleChange = e => {
-    this.setState({
-      form: {
-        ...this.state.form,
-        [e.target.name]: e.target.value,
-      },
-    });
-  };
-
+class BadgeNew extends Component {
   render() {
     return (
-      <React.Fragment>
+      <div>
+        <Navbar />
         <div className="BadgeNew__hero">
           <img className="img-fluid" src={header} alt="Logo" />
         </div>
@@ -35,26 +17,15 @@ class BadgeNew extends React.Component {
         <div className="container">
           <div className="row">
             <div className="col-6">
-              <Badge
-                firstName={this.state.form.firstName}
-                lastName={this.state.form.lastName}
-                twitter={this.state.form.twitter}
-                jobTitle={this.state.form.jobTitle}
-                email={this.state.form.email}
-                avatarUrl="https://www.gravatar.com/avatar/21594ed15d68ace3965642162f8d2e84?d=identicon"
-              />
+              <Badge firstName="Jose" lastName="Vásquez" jobTitle="Microbiologist" twitter="@jvasquez" avatarUrl="https://www.gravatar.com/avatar?d=identicon" />
             </div>
-
             <div className="col-6">
-              <BadgeForm
-                onChange={this.handleChange}
-                formValues={this.state.form}
-              />
+              <BadgeForm />
             </div>
           </div>
         </div>
-      </React.Fragment>
-    );
+      </div>
+    )
   }
 }
 
