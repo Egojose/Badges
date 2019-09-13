96b7b12f22fbec137786a0c9d6655f7cb38de5db
Egojose <josemicrobiologo@gmail.com>
2019-09-13 14:21:33 -0500

    se renderiza Badges

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Badge from './components/Badge';
 import BadgeNew from './pages/BadgeNew';
+import Badges from './pages/Badges'
 import 'bootstrap/dist/css/bootstrap.css';
 import './global.css';
 
 
 const container = document.getElementById("app");
 
-ReactDOM.render(<BadgeNew 
+ReactDOM.render(<Badges 
     // firstName ="Jose" 
     // lastName ="Vásquez" 
     // avatarUrl ="https://www.gravatar.com/avatar?d=identicon"
