3b4b7f03cfe82e08370891f505ed5c5d399bfcb1
Egojose <josemicrobiologo@gmail.com>
2019-09-13 14:22:08 -0500

    se modifican para render en index.js

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -6,6 +6,26 @@ import './styles/BadgeNew.css'
 import Navbar from '../components/Navbar';
 
 class BadgeNew extends Component {
+  state = {
+    form: {
+      firstName: '',
+      lastName: '',
+      email: '',
+      jobTitle: '',
+      twitter: ''
+    }
+  }
+
+  handleChange = e => {
+    // const nextForm = this.state.form;
+    // nextForm[e.target.name] = e.target.value;
+    this.setState({
+      form: {
+        ...this.state.form,
+        [e.target.name]: e.target.value
+      }
+    })
+  }
   render() {
     return (
       <div>
@@ -17,10 +37,19 @@ class BadgeNew extends Component {
         <div className="container">
           <div className="row">
             <div className="col-6">
-              <Badge firstName="Jose" lastName="Vásquez" jobTitle="Microbiologist" twitter="@jvasquez" avatarUrl="https://www.gravatar.com/avatar?d=identicon" />
+              <Badge 
+              firstName={this.state.form.firstName} 
+              lastName={this.state.form.lastName} 
+              email={this.state.form.email}
+              jobTitle={this.state.form.jobTitle} 
+              twitter={this.state.form.twitter} 
+              avatarUrl="https://www.gravatar.com/avatar?d=identicon" />
             </div>
             <div className="col-6">
-              <BadgeForm />
+              <BadgeForm
+                onChange={this.handleChange}
+                formValues={this.state.form}
+              />
             </div>
           </div>
         </div>
